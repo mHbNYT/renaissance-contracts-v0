ae847bc6036fc52c09ed713f187fe29509fc7ffa
Colin Nielsen <colinrnielsen@gmail.com>
2022-02-11 15:38:30 -0700

    update balanceOf function

--- contracts/fNftERC20.sol
+++ contracts/fNftERC20.sol
@@ -92,7 +92,7 @@ contract FNFTERC20 is ERC20, ERC721Holder {
     address public creator;
     uint256 public bidThreshold;
     uint256 public redeemThreshold;
-    address public redeemBlacklist;
+    address public redemptionBlacklist;
 
     bool public liquidated;
     bool public bidAccepted;
@@ -106,7 +106,7 @@ contract FNFTERC20 is ERC20, ERC721Holder {
     /// @param _fractions the amount of fractions to create
     /// @param _bidThreshold the bid acceptance threshold % necessary to accept a bid on an fNFT (e.g: 50 = 50%)
     /// @param _redeemThreshold the % of the totalSupply you have to hold to redeem the token automatically
-    /// @param _redeemBlacklist an optional address you can blacklist from the redemption process:
+    /// @param _redemptionBlacklist an optional address you can blacklist from the redemption process:
     ///         (e.g: if the treasury holds 50%, you may want to ignore the treasury's balance when calculating the what % of the totalSupply a redeemer needs to hold)
     constructor(
         IUniswapV2Factory _factory,
@@ -115,7 +115,7 @@ contract FNFTERC20 is ERC20, ERC721Holder {
         uint256 _fractions,
         uint8 _bidThreshold,
         uint8 _redeemThreshold,
-        address _redeemBlacklist
+        address _redemptionBlacklist
     )
         ERC20(
             string(abi.encodePacked("Fractionalized ", _nft.name())),
@@ -136,7 +136,7 @@ contract FNFTERC20 is ERC20, ERC721Holder {
         tokenId = _tokenId;
         bidThreshold = _bidThreshold;
         redeemThreshold = _redeemThreshold;
-        redeemBlacklist = _redeemBlacklist;
+        redemptionBlacklist = _redemptionBlacklist;
         creator = msg.sender;
         _mint(msg.sender, _fractions);
 
@@ -188,8 +188,9 @@ contract FNFTERC20 is ERC20, ERC721Holder {
     /// @notice Allows a user to redeem if they hold > redeemThreshold of the totalSupply
     /// @dev User must transfer: fNFT price (in eth) * (totalSupply - balance[msg.sender]) to payout the remaining holders
     function redeem() external payable {
+        uint256 userBalance = balanceOf(msg.sender);
         // ensure the user's current balance is above the redemption threshold %
-        if ((balanceOf(msg.sender) * 100) / totalSupply < redeemThreshold)
+        if ((userBalance * 100) / totalSupply() < redeemThreshold)
             revert NotEnoughTokens();
         // prevent a user from redeeming when a bid is accepted
         if (bidAccepted) revert BidAccepted();
@@ -204,8 +205,8 @@ contract FNFTERC20 is ERC20, ERC721Holder {
         // NOTE: MINIMUM_LIQUIDITY represents the small amount of tokens that are transferred to address(0) (i.e: locked forever)
         //      on the creation of a uni liquidity pool, we should not ask the redeemer to payback address(0) :)
         uint256 tokensToPayout = totalSupply() -
-            (redeemBlacklist == address(0) ? 0 : balanceOf(redeemBlacklist)) -
-            balanceOf(msg.sender) -
+            (redemptionBlacklist == address(0) ? 0 : balanceOf(redemptionBlacklist)) -
+            userBalance -
             IUniswapV2Pair(pair).MINIMUM_LIQUIDITY();
 
         // calculate the price in eth that the redeemer needs to pay the remaining holders
@@ -240,7 +241,7 @@ contract FNFTERC20 is ERC20, ERC721Holder {
         // revert if the pair doesn't exist (e.g: the the LP hasn't been deployed yet)
         if (pair == address(0)) revert NoV2Pair();
         // sort the tokens by order so we can query the for this fNFT price in comparison to the ETH price
-        (address token0, address token1) = address(this) < WETH
+        (address token0, ) = address(this) < WETH
             ? (address(this), WETH)
             : (WETH, address(this));
 
