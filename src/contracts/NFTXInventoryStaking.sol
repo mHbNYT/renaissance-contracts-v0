d61287238c44a89568cb356864698fd85b28ea49
0xkowloon <0xkowloon@protonmail.com>
2022-05-28 16:19:31 +0100

    chore: use OZ IERC20Metadata

--- src/contracts/NFTXInventoryStaking.sol
+++ src/contracts/NFTXInventoryStaking.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
 
 import "./interfaces/INFTXVaultFactory.sol";
 import "./interfaces/INFTXVault.sol";
 import "./interfaces/INFTXInventoryStaking.sol";
-import "./token/IERC20Metadata.sol";
 import "./util/NFTXPausableUpgradeable.sol";
 import "./proxy/BeaconUpgradeable.sol";
 import "./proxy/Create2BeaconProxy.sol";
--- src/contracts/StakingTokenProvider.sol
+++ src/contracts/StakingTokenProvider.sol
@@ -6,8 +6,7 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-
-import "./token/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract StakingTokenProvider is OwnableUpgradeable {
 
