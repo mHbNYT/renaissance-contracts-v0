61c8ed77c271298415fd0cd5c9eb03685ef6e21c
0xluckyg <scottsgcho@gmail.com>
2022-07-30 17:13:17 +0900

    fix: cache WETH in LPStaking

--- src/contracts/LPStakingZap.sol
+++ src/contracts/LPStakingZap.sol
@@ -28,8 +28,9 @@ contract LPStakingZap is ILPStakingZap, Ownable, ReentrancyGuard {
   constructor(address _vaultManager, address _router) Ownable() ReentrancyGuard() {
     router = IUniswapV2Router(_router);
     vaultManager = IVaultManager(_vaultManager);
-    WETH = IWETH(IUniswapV2Router(_router).WETH());
-    IERC20Upgradeable(address(IUniswapV2Router(_router).WETH())).safeApprove(_router, type(uint256).max);
+    IWETH weth = IWETH(IUniswapV2Router(_router).WETH());
+    WETH = weth;
+    IERC20Upgradeable(address(weth)).safeApprove(_router, type(uint256).max);
   }
 
   function assignLPStakingContract() public override {
